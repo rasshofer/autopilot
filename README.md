3370aa5fd841bf8fd1d00c206655a15e60307357
Thomas Rasshofer <hello@thomasrasshofer.com>
2017-12-18 00:34:50 +0100

    Implement support for GDAX

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Coinvestor
 
-> A simple (CLI) tool to »Cost Average«-buy Bitcoin, Litecoin, and/or Ethereum using Coinbase
+> A simple (CLI) tool to »Cost Average«-buy Bitcoin, Litecoin, and/or Ethereum using Coinbase or GDAX
 
 Cost averaging is a common investment technique of buying a fixed amount of a particular investment (e.g. cryptocurrencies) on a regular schedule, regardless of the share price. Hence one purchases more shares when prices are low and fewer shares when prices are high. This way one is able to compensate for exchange rate fluctuations.
 
@@ -69,6 +69,33 @@ Example: `NlpFapHtwkqyOfg`
 Your Coinbase API secret. 32 characters long.
 
 Example: `bchx51JunqVeEH2LG2li7iKDWMj1lrVo`
+
+### GDAX account
+
+Besides Coinbase you’re also able to buy coins using GDAX. In order to use Coinvestor with GDAX, you need to create a new API key for your GDAX account. To do so, head over to [https://www.gdax.com/settings/api](https://www.gdax.com/settings/api) and create a new key. Coinvestor needs the following permissions in order to perform buys for you.
+
+- `View`
+- `Trade`
+
+Make sure you granted all of these permissions for the API key.
+
+#### `gdax_api_key`
+
+Your GDAX API key. 32 characters long.
+
+Example: `1ab05e21dc847c87970130229b61a987`
+
+#### `gdax_api_secret`
+
+Your GDAX API secret.
+
+Example: `AwtyoyLzp3MyDGxuJvj2nGSTWR1mBHMDIxybMH1YqJcsYwOaDIH6JQxbDGLdFmWIKl0bDwcaHJ56JRVaM2gsYD==`
+
+#### `coinbase_api_passphrase`
+
+Your GDAX API passphrase. 11 characters long.
+
+Example: `johxudf2va4`
 
 ### Purchases
 
---
+++ services/gdax.js
@@ -0,0 +1,88 @@
+const gdax = require('gdax');
+const leprechaun = require('leprechaun');
+const roundTo = require('round-to');
+
+module.exports = (config) => {
+
+  const apiURI = 'https://api.gdax.com';
+  const sandboxURI = 'https://api-public.sandbox.gdax.com';
+  const currencies = [
+    'btc',
+    'eth',
+    'ltc'
+  ].filter((currency) => (config[currency] && config[currency] > 0));
+
+  const client = new gdax.AuthenticatedClient(config.gdax_api_key, config.gdax_api_secret, config.gdax_api_passphrase, apiURI);
+
+  leprechaun.info(`Using ${config.currency.toUpperCase()} as currency`);
+
+  client.getAccounts((err, response, accounts) => {
+
+    if (err) {
+      leprechaun.error('Error while fetching accounts');
+      leprechaun.error(err);
+      process.exit(1);
+    }
+
+    leprechaun.info(`Fetched ${accounts.length} ${accounts.length === 1 ? 'account' : 'accounts'}`);
+
+    const wallet = accounts.find((account) => account.currency.toLowerCase() === config.currency.toLowerCase());
+
+    if (!wallet) {
+      leprechaun.error(`Error while fetching your ${config.currency.toUpperCase()} wallet for payment`);
+      process.exit(1);
+    }
+
+    const balance = parseFloat(wallet.balance, 10);
+    const total = currencies.reduce((sum, currency) => sum + parseFloat(config[currency], 10), 0);
+
+    leprechaun.info(`Using »${wallet.id}« for payments`);
+    leprechaun.info(`Current balance: ${roundTo(balance, 2)} ${wallet.currency.toUpperCase()}`);
+
+    if (total > balance) {
+      leprechaun.warning(`Current balance exceeds scheduled total (= ${roundTo(total, 2)} ${wallet.currency.toUpperCase()})`);
+    }
+
+    currencies.forEach((currency) => {
+
+      const amount = parseFloat(config[currency], 10);
+      const publicClient = new gdax.PublicClient(`${currency.toUpperCase()}-${config.currency.toUpperCase()}`, apiURI);
+
+      publicClient.getProduct24HrStats((err, response, stats) => {
+
+        if (err) {
+          leprechaun.error(`Error while fetching buy prices for ${currency.toUpperCase()}`);
+          leprechaun.error(err);
+          process.exit(1);
+        }
+
+        const price = parseFloat(stats.last, 10);
+        const buy = roundTo(amount / price, 8);
+
+        leprechaun.info(`Buy price for ${currency.toUpperCase()} is ${price} ${config.currency.toUpperCase()}`);
+
+        const props = {
+          price: price,
+          size: buy,
+          product_id: `${currency.toUpperCase()}-${config.currency.toUpperCase()}`
+        };
+
+        client.buy(props, (err, response, order) => {
+
+          if (err) {
+            leprechaun.error(`Error while buying ${currency.toUpperCase()}`);
+            leprechaun.error(err);
+            process.exit(1);
+          }
+
+          leprechaun.success(`Placed buy order for ${order.size} ${currency.toUpperCase()} (= ${(order.size * order.price)} ${config.currency.toUpperCase()})`);
+
+        });
+
+      });
+
+    });
+
+  });
+
+};
